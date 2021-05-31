a7388690483e0283e43b5f2c7ea6c16e987e861a
Eulália Ribeiro <eulalia.ribeiro@lais.huol.ufrn.br>
2021-05-31 15:29:40 -0300

    modal dos termos e política de privacidade

--- src/components/UserRegisterForm/index.js
+++ src/components/UserRegisterForm/index.js
@@ -8,7 +8,8 @@ const UserRegisterForm = () => {
     const [initial, setInicial] = useState(true)
     const [validateRegister, setValidateRegister] = useState(false)
     const [handleClick, setHandleClick] = useState(false)
-    
+
+    let modal = document.getElementById("terms-modal")    
 
     const validateEmail = (e) => {
       var email = e.target.value
@@ -20,6 +21,10 @@ const UserRegisterForm = () => {
       }
       setInicial(false)
     }
+
+    /* const openModal = () => {
+        setModal(false)
+    } */
 
     //  TODO: validar campos do formulário
     // nome só pode ser letra
@@ -73,10 +78,38 @@ const UserRegisterForm = () => {
             </form>
 
             { (validateRegister && handleClick) && 
-                <p>
-                    Ao assinar você concorda com os <a href="/">termos de serviço</a> e <a href="/">política de privacidade</a>
+                <p className="terms-and-politics">
+                    Ao assinar você concorda com os <button id="terms-button" onClick={() => modal.style.display="block"}>termos de serviço</button> e <button id="politics-button" onClick={() => modal.style.display="block"}>política de privacidade</button>
                 </p>
             }
+
+            <div id="terms-modal">
+                <div className="terms-modal-content">
+                    <p className="terms-modal-title">
+                        Termos e Política de Privacidade
+                    </p>
+                    
+                    <p className="terms-modal-text">
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                        <br/>
+                        <br/>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                        <br/>
+                        <br/>
+                        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                    </p>
+
+                    <div id="terms-modal-close-button" onClick={() => modal.style.display="none"}>
+                        <p>
+                            Fechar
+                        </p> 
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
